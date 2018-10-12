a941a04f53ea87ae72bf280fe7b772792d00bc55
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-10-12 15:55:42 +0530

    Code for moving Servos in loop method

--- BlindReader ArduinoCODE/Arduino Code/src/main.cpp
+++ BlindReader ArduinoCODE/Arduino Code/src/main.cpp
@@ -25,4 +25,64 @@ void setup() {
 
 void loop() {
     // put your main code here, to run repeatedly:
+    if(Serial.available()){
+        val = analogRead(A0) 
+        if(val > 5){
+        Serial.println("k");
+        delay(500);
+    }
+        else{
+            char ch = Serial.read();
+            if(ch == ',') {
+                data+="";
+            }
+            else{
+                data+=ch;
+            }
+            //Serial.println(data);
+            if(data.length() ==6){
+                if(data.charAt(0) == '1'){
+                    s0.write(160);
+                    digitalWrite(led0, HIGH);
+                }
+                else{
+                    digitalWrite(led0, LOW)
+                    s0.write(60)
+                }
+                if(data.charAt(1) == '1'){
+                    digitalWrite(led1, HIGH)
+                    s1.write(160)
+                }else {
+                    digitalWrite(led1, LOW)
+                    s1.write(60)
+                }
+                if(data.charAt(2)== '1'){
+                    digitalWrite(led2, HIGH)
+                    s2.write(160)
+                }else {
+                    digitalWrite(led2, LOW)
+                    s2.write(60)
+                }if(data.charAt(3)== '1'){
+                    digitalWrite(led3, HIGH)
+                    s3.write(160)
+                }else {
+                    digitalWrite(led3, LOW)
+                    s3.write(60)
+                }if(data.charAt(4)== '1'){
+                    digitalWrite(led4, HIGH)
+                    s4.write(160)
+                }else {
+                    digitalWrite(led4, LOW)
+                    s4.write(60)
+                }if(data.charAt(5)== '1'){
+                    digitalWrite(led5, HIGH)
+                    s5.write(160)
+                }else {
+                    digitalWrite(led5, LOW)
+                    s5.write(60)
+                }
+            }
+
+    }}
+
 }
