2fae9f01c7f5c47cade7248f43b0b70f87bbae93
phantom5 <rickrudra@gmail.com>
2018-10-12 14:49:38 +0530

    getspeech pending

--- BlindReader App/app/src/main/java/com/example/rudra/androidapp/MainActivity.java
+++ BlindReader App/app/src/main/java/com/example/rudra/androidapp/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.rudra.androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button imageToText,speechToText,button2,button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageToText=findViewById(R.id.imageToText);
+        speechToText=findViewById(R.id.speechToText);
+        button2=findViewById(R.id.button2);
+        button4=findViewById(R.id.button4);
+        imageToText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,BlindReader.class);
+                startActivity(i);
+            }
+        });
+        speechToText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,AudiotoText.class);
+                startActivity(i);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
