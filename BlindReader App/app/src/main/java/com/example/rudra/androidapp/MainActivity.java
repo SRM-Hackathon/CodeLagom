03349c046eebdc7b414ad847a3b5a89a44160e1a
phantom5 <rickrudra@gmail.com>
2018-10-12 14:58:36 +0530

    main activity seems complete

--- BlindReader App/app/src/main/java/com/example/rudra/androidapp/MainActivity.java
+++ BlindReader App/app/src/main/java/com/example/rudra/androidapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.rudra.androidapp;
 
 import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     Button imageToText,speechToText,button2,button4;
@@ -27,8 +33,14 @@ public class MainActivity extends AppCompatActivity {
         speechToText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(MainActivity.this,AudiotoText.class);
+                Intent i=new Intent(MainActivity.this,AudioToText.class);
                 startActivity(i);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSpeechInput();
             }
         });
         button4.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +50,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void getSpeechInput()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if(resultCode == RESULT_OK && data != null)
+                {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    if(result.get(0).contains("image to text")) {
+                        Intent i = new Intent(MainActivity.this, BlindReader.class);
+                        startActivity(i);
+                    }
+                    else if(result.get(0).contains("speech to text")) {
+                        Intent i1 = new Intent(MainActivity.this, AudioToText.class);
+                        startActivity(i1);
+                    }
+                    else if(result.get(0).contains("exit")){
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
     }
 }
